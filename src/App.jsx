991e80d486a2484fce1efd6822f4026ce208d379
Cyrus Huneycutt <chuneycu@gmail.com>
2025-12-01 09:37:49 -0500

    project setup + react, vite configs, sample components + scss, bootstrap basics

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar.jsx'
+import Main from './components/Main.jsx'
+// Scss stylesheet
+import './assets/scss/styles.scss';
+
+function App() {
+  return (
+    <>
+        <Navbar />
+        <Main />
+    </>
+  )
+}
+
+export default App
